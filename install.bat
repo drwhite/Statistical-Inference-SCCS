a79bcaaeb972a73202ddee784c25911815cba9fa
Doug White <drwhite2@gmail.com>
2010-09-19 13:08:19 -0700

    fix install script

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 set sccs_tarball=sccs_1.0.tar.gz
-R CMD check %sccs_tarball%
-R CMD build %sccs_tarball%
+R CMD check sccs
+R CMD build sccs
 R CMD INSTALL %sccs_tarball%
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sccs_tarball=sccs_1.0.tar.gz
-R CMD check $sccs_tarball
-R CMD build $sccs_tarball
+R CMD check sccs
+R CMD build sccs
 R CMD INSTALL $sccs_tarball
